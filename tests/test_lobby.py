12a1b48280e12c69470bb4ed173a0146ab2aa7c8
Yuriy Kehter <yuriy.kehter@gmail.com>
2023-11-17 17:25:59 +0400

    Rework fixtures

--- tests/test_lobby.py
+++ tests/test_lobby.py
@@ -3,7 +3,6 @@ from unittest.mock import Mock, call
 import pytest
 
 from lobby import (
-    CardOnTable,
     Deck,
     Lobby,
     LobbyObserver,
@@ -35,7 +34,7 @@ def setup_deck(setup_deck_size: int) -> Deck[SetupCard]:
 
 @pytest.fixture
 def punchline_deck_size() -> int:
-    # TODO: I want to control hand size
+    # TODO: I want to control the "hand size"
     return 100
 
 
@@ -47,24 +46,22 @@ def punchline_deck(punchline_deck_size: int) -> Deck[PunchlineCard]:
 
 
 @pytest.fixture
-def observer() -> LobbyObserver:
+def observer() -> Mock:
     return Mock(LobbyObserver)
 
 
 @pytest.fixture
 def egor() -> Player:
-    player = Player(
+    return Player(
         profile=Profile(name="egor", emoji="🍎", background_color="#ff0000"),
     )
-    return player
 
 
 @pytest.fixture
 def anton() -> Player:
-    player = Player(
+    return Player(
         profile=Profile(name="anton", emoji="🍎", background_color="#ff0000"),
     )
-    return player
 
 
 @pytest.fixture
@@ -74,195 +71,143 @@ def yura() -> Player:
     )
 
 
-@pytest.fixture(name="egor connected")
+@pytest.fixture
+def lobby(
+    egor: Player,
+    setup_deck: Deck[SetupCard],
+    punchline_deck: Deck[PunchlineCard],
+    observer: Mock,
+) -> Lobby:
+    return Lobby(
+        players=[],
+        lead=egor,
+        owner=egor,
+        setups=setup_deck,
+        punchlines=punchline_deck,
+        observer=observer,  # FIXME: Looks like it's redundant
+    )
+
+
+@pytest.fixture
 def egor_connected(egor: Player, observer: Mock) -> None:
     player_mock = Mock(LobbyObserver)
     observer.attach_mock(player_mock, "egor")
     egor.observer = player_mock
 
 
-@pytest.fixture(name="anton connected")
-def anton_connected(anton: Player, observer: Mock) -> None:
+@pytest.fixture
+def anton_joined(lobby: Lobby, anton: Player) -> None:
+    lobby.add_player(anton)
+
+
+@pytest.fixture
+def anton_connected(
+    anton_joined: None, lobby: Lobby, anton: Player, observer: Mock
+) -> None:
     player_mock = Mock(LobbyObserver)
     observer.attach_mock(player_mock, "anton")
     anton.observer = player_mock
+    lobby.set_connected(anton)
 
 
-@pytest.fixture(name="yura connected")
-def yura_connected(yura: Player, observer: Mock) -> None:
+@pytest.fixture
+def yura_joined(lobby: Lobby, yura: Player) -> None:
+    lobby.add_player(yura)
+
+
+@pytest.fixture
+def yura_connected(
+    yura_joined: None, lobby: Lobby, yura: Player, observer: Mock
+) -> None:
     player_mock = Mock(LobbyObserver)
     observer.attach_mock(player_mock, "yura")
     yura.observer = player_mock
+    lobby.set_connected(yura)
 
 
+@pytest.mark.usefixtures("anton_joined", "yura_joined")
 def test_lobby_change_lead(
-    egor: Player,
-    anton: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: LobbyObserver,
+    lobby: Lobby, egor: Player, anton: Player, yura: Player
 ) -> None:
-    lobby = Lobby(
-        players=[egor, anton],
-        lead=yura,
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,  # FIXME: Looks like it's redundant
-    )
-    lobby.change_lead()
     assert lobby.players == [anton, yura]
-    assert lobby.lead is egor
+    lobby.change_lead()
+    assert lobby.players == [yura, egor]
+    assert lobby.lead is anton
 
 
-def test_lobby_change_setup_card(
-    egor: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: LobbyObserver,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,  # FIXME: Looks like it's redundant
-    )
-    new_setup_card = SetupCard(
-        text="new setup card", case="", start_with_punchline=False
-    )
-    setup_deck.cards.append(new_setup_card)
-    lobby.change_setup_card(new_setup_card)
-    assert lobby.setup_card is new_setup_card
+def test_lobby_change_setup_card(lobby: Lobby, setup_deck: Deck[SetupCard]) -> None:
+    setup_card = setup_deck.get_random_card()
+    lobby.change_setup_card(setup_card)
+    assert lobby.setup_card is setup_card
 
 
+@pytest.mark.usefixtures("anton_joined")
 def test_lobby_choose_punchline_card(
-    egor: Player,
-    anton: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: LobbyObserver,
+    lobby: Lobby, anton: Player, punchline_deck: Deck[PunchlineCard]
 ) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[anton],
-        owner=anton,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,  # FIXME: It's annoying to provide it every time
-    )
     punchline_card = punchline_deck.get_random_card()
     anton.punchline_cards.append(punchline_card)
+    # TODO: Maybe move this method to player?
+
     lobby.choose_punchline_card(anton, punchline_card)
-    assert punchline_card is lobby.get_card_from_table(punchline_card).card
+
+    card_on_table = lobby.get_card_from_table(punchline_card)
+    assert card_on_table.card is punchline_card
+    assert card_on_table.player is anton
+    assert not card_on_table.is_open
     assert punchline_card not in anton.punchline_cards
 
 
+@pytest.mark.usefixtures("anton_joined")
 def test_lobby_choose_punchline_member_not_punchline_holder(
-    egor: Player,
-    anton: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: LobbyObserver,
+    lobby: Lobby, egor: Player, anton: Player, punchline_deck: Deck[PunchlineCard]
 ) -> None:
-    lobby = Lobby(
-        lead=anton,
-        players=[egor],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,  # FIXME: It's annoying to provide it every time
-    )
     punchline_card = punchline_deck.get_random_card()
     egor.punchline_cards.append(punchline_card)
     with pytest.raises(PlayerNotPunchlineCardHolderError):
         lobby.choose_punchline_card(anton, punchline_card)
 
 
+@pytest.mark.usefixtures("anton_joined")
 def test_open_punchline_card(
-    egor: Player,
-    anton: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: LobbyObserver,
+    lobby: Lobby, egor: Player, anton: Player, punchline_deck: Deck[PunchlineCard]
 ) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[anton],
-        owner=anton,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,  # FIXME: It's annoying to provide it every time
-    )
+    punchline_card = punchline_deck.get_random_card()
+    anton.punchline_cards.append(punchline_card)
+    lobby.choose_punchline_card(anton, punchline_card)
+
+    lobby.open_punchline_card(egor, punchline_card)
+
+    card_on_table = lobby.get_card_from_table(punchline_card)
+    assert card_on_table.is_open
+
+
+@pytest.mark.usefixtures("anton_joined")
+def test_open_punchline_card_member_not_lead(
+    lobby: Lobby, anton: Player, punchline_deck: Deck[PunchlineCard]
+) -> None:
+    punchline_card = punchline_deck.get_random_card()
+    # TODO: Use `Lead` instead of `Dealer`
+    with pytest.raises(PlayerNotDealerError):
+        lobby.open_punchline_card(anton, punchline_card)
+
+
+@pytest.mark.usefixtures("anton_joined")
+def test_lobby_lead_choose_punchline_card(
+    lobby: Lobby, egor: Player, anton: Player, punchline_deck: Deck[PunchlineCard]
+) -> None:
     punchline_card = punchline_deck.get_random_card()
     anton.punchline_cards.append(punchline_card)
     lobby.choose_punchline_card(anton, punchline_card)
     lobby.open_punchline_card(egor, punchline_card)
-    assert isinstance(lobby.table[0], CardOnTable)
 
-
-def test_open_punchline_card_member_not_lead(
-    egor: Player,
-    anton: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: LobbyObserver,
-) -> None:
-    lobby = Lobby(
-        lead=anton,
-        players=[egor],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,  # FIXME: It's annoying to provide it every time
-    )
-    punchline_card = punchline_deck.get_random_card()
-    with pytest.raises(PlayerNotDealerError):
-        lobby.open_punchline_card(egor, punchline_card)
-
-
-def test_lobby_lead_choose_punchline_card(
-    egor: Player,
-    anton: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: LobbyObserver,
-) -> None:
-    lobby = Lobby(
-        lead=anton,
-        players=[egor],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,  # FIXME: It's annoying to provide it every time
-    )
-    punchline_card = punchline_deck.get_random_card()
-    egor.punchline_cards.append(punchline_card)
-    lobby.choose_punchline_card(egor, punchline_card)
-    lobby.open_punchline_card(anton, punchline_card)
     lobby.lead_choose_punchline_card(punchline_card)
-    assert egor.score == 1
+
+    assert anton.score == 1
 
 
-@pytest.mark.usefixtures("egor connected")
-def test_player_joined(
-    egor: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: Mock,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,
-    )
-    # FIXME: I want to test which player has received the message
+@pytest.mark.usefixtures("egor_connected")
+def test_player_joined(lobby: Lobby, yura: Player, observer: Mock) -> None:
     lobby.add_player(yura)
 
     expected_events = [
@@ -271,95 +216,8 @@ def test_player_joined(
     observer.assert_has_calls(expected_events)
 
 
-@pytest.mark.usefixtures("yura connected")
-def test_player_not_received_self_joined(
-    egor: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: Mock,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,
-    )
-    lobby.add_player(yura)
-
-    unexpected_events = [
-        call.yura.player_joined(yura),
-    ]
-    assert not any(event in observer.mock_calls for event in unexpected_events)
-
-
-@pytest.mark.usefixtures("egor connected", "yura connected")
-def test_player_disconnected(
-    egor: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: Mock,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,
-    )
-    lobby.add_player(yura)
-    lobby.set_connected(yura)
-
-    lobby.set_disconnected(yura)
-
-
-@pytest.mark.usefixtures("egor connected", "yura connected")
-def test_player_disconnected(
-    egor: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: Mock,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,
-    )
-    lobby.add_player(yura)
-    lobby.set_connected(yura)
-
-    lobby.set_disconnected(yura)
-
-    expected_events = [
-        call.egor.player_disconnected(yura),
-    ]
-    observer.assert_has_calls(expected_events)
-
-
-@pytest.mark.usefixtures("egor connected")
-def test_player_connected(
-    egor: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: Mock,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[yura],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,
-    )
+@pytest.mark.usefixtures("egor_connected", "yura_joined")
+def test_player_connected(lobby: Lobby, yura: Player, observer: Mock) -> None:
     lobby.set_connected(yura)
 
     expected_events = [
@@ -368,23 +226,18 @@ def test_player_connected(
     observer.assert_has_calls(expected_events)
 
 
-@pytest.mark.usefixtures("egor connected", "yura connected")
-def test_owner_start_game(
-    egor: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: Mock,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[yura],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,
-    )
+@pytest.mark.usefixtures("egor_connected", "yura_connected")
+def test_player_disconnected(lobby: Lobby, yura: Player, observer: Mock) -> None:
+    lobby.set_disconnected(yura)
 
+    expected_events = [
+        call.egor.player_disconnected(yura),
+    ]
+    observer.assert_has_calls(expected_events)
+
+
+@pytest.mark.usefixtures("egor_connected", "yura_connected")
+def test_owner_start_game(lobby: Lobby, egor: Player, observer: Mock) -> None:
     lobby_settings = LobbySettings(turn_duration=30)
     lobby.start_game(egor, lobby_settings)
 
@@ -395,23 +248,8 @@ def test_owner_start_game(
     observer.assert_has_calls(expected_events, any_order=True)
 
 
-@pytest.mark.usefixtures("egor connected", "yura connected")
-def test_not_owner_start_game(
-    egor: Player,
-    yura: Player,
-    setup_deck: Deck[SetupCard],
-    punchline_deck: Deck[PunchlineCard],
-    observer: Mock,
-) -> None:
-    lobby = Lobby(
-        lead=egor,
-        players=[yura],
-        owner=egor,
-        setups=setup_deck,
-        punchlines=punchline_deck,
-        observer=observer,
-    )
-
+@pytest.mark.usefixtures("egor_connected", "yura_connected")
+def test_not_owner_start_game(lobby: Lobby, yura: Player, observer: Mock) -> None:
     lobby_settings = LobbySettings(turn_duration=30)
     with pytest.raises(PlayerNotOwnerError):
         lobby.start_game(yura, lobby_settings)
