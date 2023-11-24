b39fd6fb20fe757298d7d222259516738055e5ca
Yuriy Kehter <yuriy.kehter@gmail.com>
2023-11-24 21:55:04 +0400

    Add fixture

--- tests/test_states.py
+++ tests/test_states.py
@@ -1,9 +1,6 @@
-import pytest
-
 from lobby import Lobby
 
 
-@pytest.mark.usefixtures("state_gathering")
 def test_transit_gathering_to_turns(lobby: Lobby) -> None:
     lobby.state.start_turn()
     isinstance(lobby.state, Turns)
