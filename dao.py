6e23a01f3986680ed59cd0578a275e677cab7481
amolodykh <antonmolodykh96@gmail.com>
2023-12-06 23:22:02 +0400

    Implements CardsDAO

--- dao.py
+++ dao.py
@@ -1,15 +1,42 @@
 from uuid import uuid4
 
-from models import Punchline
+from sqlalchemy import select
+
+from db import async_session
+from models import Punchline, Setup
 from lobby import Deck, PunchlineCard, SetupCard
 
 
 class CardsDAO:
-    def get_setups(self, deck_id: str) -> Deck[SetupCard]:
-        return setups_deck
+    async def get_setups(self, deck_id: str) -> Deck[SetupCard]:
+        async with async_session() as session:
+            result = await session.execute(select(Setup))
 
-    def get_punchlines(self, deck_id: str) -> Deck[PunchlineCard]:
-        return punchlines_deck
+            return Deck(
+                cards=[
+                    SetupCard(
+                        id=row.id,
+                        text=row.text,
+                        case=row.variant,
+                        starts_with_punchline=row.starts_with_punchline,
+                    )
+                    for row in result.all()
+                ]
+            )
+
+    async def get_punchlines(self, deck_id: str) -> Deck[PunchlineCard]:
+        async with async_session() as session:
+            result = await session.execute(select(Punchline))
+
+            return Deck(
+                cards=[
+                    PunchlineCard(
+                        id=row.id,
+                        text=row.text,
+                    )
+                    for row in result.all()
+                ]
+            )
 
 
 cards_dao = CardsDAO()
