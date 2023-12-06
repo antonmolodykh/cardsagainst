f952b2242740a431ea089d85f8702b1806d9886f
Yuriy Kehter <yuriy.kehter@gmail.com>
2023-12-07 01:01:32 +0400

    Fix tests

--- tests/test_states.py
+++ tests/test_states.py
@@ -44,13 +44,13 @@ async def test_transit_judgement_to_finished(
     anton: Player,
     punchline_deck: Deck[PunchlineCard],
     setup_deck: Deck[SetupCard],
-    punchline_card: PunchlineCard,
 ) -> None:
     setup_card = setup_deck.get_card()
     lobby.add_player(anton)
     lobby.settings.winner_score = 1
     lobby.settings.finish_delay = 0
     lobby.transit_to(Judgement(setup_card))
+    punchline_card = punchline_deck.get_card()
     anton.hand.append(punchline_card)
     lobby.state.choose_punchline_card(anton, punchline_card)
     lobby.state.pick_turn_winner(punchline_card)
