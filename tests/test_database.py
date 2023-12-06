50a1d0c5823e1251e853064fc8262566017cfe15
Yuriy Kehter <yuriy.kehter@gmail.com>
2023-12-06 23:34:23 +0400

    Add tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,20 +5,33 @@ import pytest
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from dao import cards_dao
+from dao import CardsDAO
 from lobby import PunchlineCard, SetupCard
 from models import Punchline, Setup, metadata
 
 
 @pytest.fixture
-async def session() -> AsyncGenerator[AsyncSession, None]:
+async def async_session():
     engine = create_async_engine(os.environ["PG_DATABASE"])
     async with engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
 
-    async_session = async_sessionmaker(engine)
+    return async_sessionmaker(engine)
+
+
+@pytest.fixture
+async def session(async_session) -> AsyncGenerator[AsyncSession, None]:
+    engine = create_async_engine(os.environ["PG_DATABASE"])
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+
     async with async_session() as session:
         yield session
+
+
+@pytest.fixture(autouse=True)
+async def cards_dao(session) -> CardsDAO:
+    return CardsDAO()
 
 
 @pytest.fixture
@@ -41,17 +54,22 @@ async def test_insert_punchline_card(session: AsyncSession) -> None:
 
 @pytest.mark.usefixtures("setup_card")
 async def test_insert_setup_card(session: AsyncSession) -> None:
+    result = await session.execute(select(Setup))
+    a = [row for row in result.all()]
+    assert isinstance(a[0][0].text, str)
+
     ((setup_card,),) = await session.execute(select(Setup))
-    assert isinstance(setup_card, Setup)
+    assert isinstance(a[0][0], Setup)
+    assert isinstance(a[0][0], Setup)
 
 
 @pytest.mark.usefixtures("setup_card")
-async def test_get_setups() -> None:
+async def test_get_setups(cards_dao: CardsDAO) -> None:
     deck = await cards_dao.get_setups("123")
     assert isinstance(deck.get_card(), SetupCard)
 
 
 @pytest.mark.usefixtures("punchline_card")
-async def test_get_punchlines() -> None:
+async def test_get_punchlines(cards_dao: CardsDAO) -> None:
     deck = await cards_dao.get_punchlines("123")
     assert isinstance(deck.get_card(), PunchlineCard)
