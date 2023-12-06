ad2db716e56a078f7ca9a159512e3d9b76ca08ad
Yuriy Kehter <yuriy.kehter@gmail.com>
2023-12-06 23:22:09 +0400

    Add tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,13 +5,18 @@ import pytest
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from models import Punchline, Setup
+from dao import cards_dao
+from lobby import PunchlineCard, SetupCard
+from models import Punchline, Setup, metadata
 
 
 @pytest.fixture
 async def session() -> AsyncGenerator[AsyncSession, None]:
     engine = create_async_engine(os.environ["PG_DATABASE"])
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+
+    async_session = async_sessionmaker(engine)
     async with async_session() as session:
         yield session
 
@@ -38,3 +43,15 @@ async def test_insert_punchline_card(session: AsyncSession) -> None:
 async def test_insert_setup_card(session: AsyncSession) -> None:
     ((setup_card,),) = await session.execute(select(Setup))
     assert isinstance(setup_card, Setup)
+
+
+@pytest.mark.usefixtures("setup_card")
+async def test_get_setups() -> None:
+    deck = await cards_dao.get_setups("123")
+    assert isinstance(deck.get_card(), SetupCard)
+
+
+@pytest.mark.usefixtures("punchline_card")
+async def test_get_punchlines() -> None:
+    deck = await cards_dao.get_punchlines("123")
+    assert isinstance(deck.get_card(), PunchlineCard)
